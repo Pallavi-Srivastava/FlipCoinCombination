da2fea7096f65d8ac6f63bcba08b4c539041a07e
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-09-03 02:39:22 +0530

    Doublet Combination

--- FlipCoinUC3.sh
+++ FlipCoinUC3.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 read -p "Enter How many times you want to flip:" n
 flips=1
-heads=0
-tails=0
+Head=0
+Tail=0
+Ht=0
+Th=0
 function coin1()
 	{
 		random1=$((RANDOM%2))
@@ -13,7 +15,6 @@ function coin2()
                 random2=$((RANDOM%2))
                 echo $random2
         }
-declare -A singlet
 declare -A doublet
 while [[ $flips -le $n ]]
 do
@@ -21,31 +22,26 @@ do
      value2="$(coin2 $random2)"
      if [[ $value1 -eq 1 && $value2 -eq 1 ]]
      then
-        echo "Heads";
-	singlet[$flips]=Head;
-	doublet[$flips]=Head,Head;
-        heads=`expr $heads + 1`;
+        echo "HH";
+	doublet[$flips]=HH;
+        Head=`expr $Head + 1`;
      elif [[ $value -eq 0 && $value2 -eq 0 ]]
      then
-         echo "Tails";
-	 singlet[$flips]=Tail;
-	 doublet[$flips]=Tail,Tail;
-         tails=`expr $tails + 1`;
+         echo "TT";
+	 doublet[$flips]=TT;
+         Tail=`expr $Tail + 1`;
      elif [[ $value -eq 1 && $value2 -eq 0 ]]
      then
-         echo "Heads,Tails";
-         singlet[$flips]=Head;
-         doublet[$flips]=Head,Tail;
-         tails=`expr $tails + 1`;
+         echo "HT";
+         doublet[$flips]=HT;
+         Ht=`expr $Ht + 1`;
       elif [[ $value -eq 0 && $value2 -eq 1 ]]
      then
-         echo "Tails,Heads";
-         singlet[$flips]=Tail;
-         doublet[$flips]=Tail,Head;
-         tails=`expr $tails + 1`;
+         echo "TH";
+         doublet[$flips]=TH;
+         Th=`expr $Th + 1`;
       fi
 flips=`expr $flips + 1`;
 done
-echo "Singlet:" "${singlet[@]}"
 echo "Doublet:" "${doublet[@]}"
 exit;
